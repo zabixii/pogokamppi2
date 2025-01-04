b773dc541e23be8a3171204af716f7a30762c8da
zabixii <ilja.sorjamaa@gmail.com>
2025-01-05 00:16:57 +0200

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,197 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+<>
+  <meta charSet="utf-8" />
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+  />
+  <link
+    rel="shortcut icon"
+    href="assets/images/kamppilogo.png"
+    type="image/webp"
+  />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="assets/images/kamppilogo.png"
+  />
+  <link rel="icon" href="assets/images/kamppilogo.png" type="image/webp" />
+  <title>PoGO Kamppi</title>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+    rel="stylesheet"
+  />
+  {/* Snowfall Background Animation */}
+  <section className="animated-background">
+    <div id="stars1" />
+    <div id="stars2" />
+    <div id="stars3" />
+  </section>
+  {/* End of Snowfall Background Animation */}
+  <div className="min-h-full flex-h-center" id="background_div">
+    <input type="hidden" defaultValue="https://bio.link" id="app-url" />
+    <input type="hidden" defaultValue="null" id="is-featured" />
+    <canvas id="bg-canvas" className="background-overlay" />
+    <div className="mt-48 page-full-wrap relative ">
+      <input type="hidden" defaultValue="creator-page" id="page-type" />
+      <meta charSet="utf-8" />
+      <link href="assets/css/style.css" rel="stylesheet" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
+      <link
+        rel="shortcut icon"
+        href="assets/images/kamppilogo.png"
+        type="image/webp"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="assets/images/kamppilogo.png"
+      />
+      <link rel="icon" href="assets/images/kamppilogo.png" type="image/webp" />
+      <title>PoGO Kamppi</title>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      {/* Snowfall Background Animation */}
+      <section className="animated-background">
+        <div id="stars1" />
+        <div id="stars2" />
+        <div id="stars3" />
+      </section>
+      {/* End of Snowfall Background Animation */}
+      <div className="min-h-full flex-h-center" id="background_div">
+        <input type="hidden" defaultValue="https://bio.link" id="app-url" />
+        <input type="hidden" defaultValue="null" id="is-featured" />
+        <canvas id="bg-canvas" className="background-overlay" />
+        <div className="mt-48 page-full-wrap relative ">
+          <input type="hidden" defaultValue="creator-page" id="page-type" />
+          <img
+            className="display-image m-auto"
+            data-src="assets/images/kamppilogo.png"
+            src="assets/images/kamppilogo.png"
+            alt=""
+          />
+          <h2 className="page-title page-text-color page-text-font mt-16 text-center">
+            Pokémon GO Kamppi, Helsinki
+          </h2>
+          <div className="mt-24">
+            <div className="page-item-wrap relative">
+              <div className="page-item flex-both-center absolute" />
+              <a
+                target="_blank"
+                className="page-item-each py-10 flex-both-center"
+                href="https://campfire.onelink.me/eBr8?af_dp=campfire://&af_force_deeplink=true&deep_link_sub1=cj1jbHVicyZjPTBhMDg0NzkxLTU2OGUtNDk3NS04YTM2LWI2NzMwMmIzZTFjOCZpPXRydWU="
+                data-id={261652}
+                data-type="page_item"
+              >
+                <img
+                  className="link-each-image"
+                  data-src="assets/images/campfire.png"
+                  src="assets/images/campfire.png"
+                  alt=""
+                />
+                <span className=" item-title text-center">
+                  Campfire: Pokémon GO Kamppi, Helsinki
+                </span>
+              </a>
+            </div>
+            <div className="page-item-wrap relative">
+              <div className="page-item flex-both-center absolute" />
+              <a
+                target="_blank"
+                className="page-item-each py-10 flex-both-center"
+                href="https://instagram.com/pogokamppi"
+                data-id={261685}
+                data-type="page_item"
+              >
+                <img
+                  className="link-each-image"
+                  data-src="assets/images/instagram.png"
+                  src="assets/images/instagram.png"
+                  alt=""
+                />
+                <span className=" item-title text-center">
+                  Instagram: @pogokamppi{" "}
+                </span>
+              </a>
+            </div>
+            <div className="page-item-wrap relative">
+              <div className="page-item flex-both-center absolute" />
+              <a
+                target="_blank"
+                className="page-item-each py-10 flex-both-center"
+                href="https://discord.com/invite/RkE7SdnXUP"
+                data-id={261685}
+                data-type="page_item"
+              >
+                <img
+                  className="link-each-image"
+                  data-src="assets/images/da.png"
+                  src="assets/images/da.png"
+                  alt=""
+                />
+                <span className=" item-title text-center">
+                  PoGO🇫🇮 Discord server
+                </span>
+              </a>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      {/*
+    A different snowfall    
+    
+    */}
+      -image" data-src="assets/images/campfire.png"
+      src="assets/images/campfire.png" alt=""/&gt;
+      <span className=" item-title text-center">
+        Campfire: Pokémon GO Kamppi, Helsinki
+      </span>
     </div>
-  );
-}
+    <div className="page-item-wrap relative">
+      <div className="page-item flex-both-center absolute" />
+      <a
+        target="_blank"
+        className="page-item-each py-10 flex-both-center"
+        href="https://instagram.com/pogokamppi"
+        data-id={261685}
+        data-type="page_item"
+      >
+        <img
+          className="link-each-image"
+          data-src="assets/images/instagram.png"
+          src="assets/images/instagram.png"
+          alt=""
+        />
+        <span className=" item-title text-center">Instagram: @pogokamppi </span>
+      </a>
+    </div>
+    <div className="page-item-wrap relative">
+      <div className="page-item flex-both-center absolute" />
+      <a
+        target="_blank"
+        className="page-item-each py-10 flex-both-center"
+        href="https://discord.com/invite/RkE7SdnXUP"
+        data-id={261685}
+        data-type="page_item"
+      >
+        <img
+          className="link-each-image"
+          data-src="assets/images/da.png"
+          src="assets/images/da.png"
+          alt=""
+        />
+        <span className=" item-title text-center">PoGO🇫🇮 Discord server</span>
+      </a>
+    </div>
+  </div>
+  {/*
+    A different snowfall    
+    
+    */}
+</>
+
